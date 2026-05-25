4d061ae0f9d2cbbf732377034b8836b47cc6ad56
will haarhoff <williamhaarhoff@gmail.com>
2026-05-25 20:34:35 +1200

    first commit

---
+++ modules/features/niri.nix
@@ -0,0 +1,37 @@
+{ self, inputs, ...}: {
+
+	flake.nixosModules.niri = {pkgs, lib, ...}: {
+		programs.niri = {
+			enable = true;
+			package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+		};
+	};
+
+	perSystem = { pkgs, lib, self', ... }: {
+		packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
+			inherit pkgs;
+			settings = {
+				spawn-at-startup = [
+					(lib.getExe self'.packages.myNoctalia)
+				];
+				input.keyboard = {
+					xkb.layout = "us,ua";
+				};
+
+				layout.gaps = 5;
+
+				binds = {
+					"Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
+					"Mod+Shift+Return".spawn-sh = 
+						"${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+					"Mod+Q".close-window = null;
+				};
+			};
+		};
+	};
+
+}
+
+
+
+
