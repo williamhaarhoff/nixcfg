b4d6c96d1160df5f9b8c18f8a903cb4c67f3d34d
will haarhoff <williamhaarhoff@gmail.com>
2026-06-09 17:16:06 +1200

    Initial niri config

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -20,13 +20,122 @@
 
 				xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 			
-				layout.gaps = 5;
+				window-rule = [ {
+						geometry-corner-radius = 6;
+						clip-to-geometry = true;
+				} ];
 
+				layout = {
+					gaps = 6;
+					focus-ring.width = 2;
+
+					preset-column-widths = [
+					 { proportion = 0.33; }
+					 { proportion = 0.5; }
+					 { proportion = 0.66; }
+					];
+
+					preset-window-heights = [
+					 { proportion = 0.33; }
+					 { proportion = 0.5; }
+					 { proportion = 0.66; }
+					];
+
+					default-column-width = {
+						proportion = 0.5;
+					};
+				};
+				
 				binds = {
 					"Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
+
 					"Mod+Shift+Return".spawn-sh = 
 						"${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
-					"Mod+Q".close-window = null;
+
+					"Mod+Q".close-window = {};
+
+          "Mod+F".maximize-column = {};
+          "Mod+G".fullscreen-window = {};
+          "Mod+Shift+F".toggle-window-floating = {};
+          "Mod+C".center-column = {};
+
+					# preset select
+					"Mod+R".switch-preset-column-width = {};
+					"Mod+Shift+R".switch-preset-column-width-back = {};
+
+					# inter monitor focus
+					"Mod+Shift+H".focus-monitor-left = {};
+          "Mod+Shift+L".focus-monitor-right = {};
+					"Mod+Shift+K".focus-monitor-up = {};
+          "Mod+Shift+J".focus-monitor-down = {};
+					"Mod+Shift+Left".focus-monitor-left = {};
+          "Mod+Shift+Right".focus-monitor-right = {};
+					"Mod+Shift+Up".focus-monitor-up = {};
+          "Mod+Shift+Down".focus-monitor-down = {};
+
+					# intra monitor focus
+          "Mod+H".focus-column-left = {};
+          "Mod+L".focus-column-right = {};
+          "Mod+K".focus-window-up = {};
+          "Mod+J".focus-window-down = {};
+          "Mod+Left".focus-column-left = {};
+          "Mod+Right".focus-column-right = {};
+          "Mod+Up".focus-window-up = {};
+          "Mod+Down".focus-window-down = {};
+
+					# inter workspace focus
+					"Mod+U".focus-workspace-down = {};
+					"Mod+I".focus-workspace-up = {};
+					"Mod+Page_Down".focus-workspace-down = {};
+					"Mod+Page_Up".focus-workspace-up = {};
+
+					# intra monitor movement
+					"Mod+BracketLeft".consume-or-expel-window-left = {};
+					"Mod+BracketRight".consume-or-expel-window-right = {};
+					"Mod+Comma".consume-window-into-column = {};
+					"Mod+Period".expel-window-from-column = {};
+
+          "Mod+Ctrl+H".move-column-left = {};
+          "Mod+Ctrl+L".move-column-right = {};
+          "Mod+Ctrl+K".move-window-up = {};
+          "Mod+Ctrl+J".move-window-down = {};
+					"Mod+Ctrl+Left".move-column-left = {};
+          "Mod+Ctrl+Right".move-column-right = {};
+          "Mod+Ctrl+Up".move-window-up = {};
+          "Mod+Ctrl+Down".move-window-down = {};
+
+					# inter monitor movement
+          "Mod+Shift+Ctrl+H".move-column-to-monitor-left = {};
+          "Mod+Shift+Ctrl+L".move-column-to-monitor-right = {};
+          "Mod+Shift+Ctrl+K".move-window-to-monitor-up = {};
+          "Mod+Shift+Ctrl+J".move-window-to-monitor-down = {};
+					"Mod+Shift+Ctrl+Left".move-column-to-monitor-left = {};
+          "Mod+Shift+Ctrl+Right".move-column-to-monitor-right = {};
+          "Mod+Shift+Ctrl+Up".move-window-to-monitor-up = {};
+          "Mod+Shift+Ctrl+Down".move-window-to-monitor-down = {};
+
+          "Mod+1".focus-workspace = "w0";
+          "Mod+2".focus-workspace = "w1";
+          "Mod+3".focus-workspace = "w2";
+          "Mod+4".focus-workspace = "w3";
+          "Mod+5".focus-workspace = "w4";
+          "Mod+6".focus-workspace = "w5";
+          "Mod+7".focus-workspace = "w6";
+          "Mod+8".focus-workspace = "w7";
+          "Mod+9".focus-workspace = "w8";
+          "Mod+0".focus-workspace = "w9";
+
+          "Mod+Shift+1".move-column-to-workspace = "w0";
+          "Mod+Shift+2".move-column-to-workspace = "w1";
+          "Mod+Shift+3".move-column-to-workspace = "w2";
+          "Mod+Shift+4".move-column-to-workspace = "w3";
+          "Mod+Shift+5".move-column-to-workspace = "w4";
+          "Mod+Shift+6".move-column-to-workspace = "w5";
+          "Mod+Shift+7".move-column-to-workspace = "w6";
+          "Mod+Shift+8".move-column-to-workspace = "w7";
+          "Mod+Shift+9".move-column-to-workspace = "w8";
+          "Mod+Shift+0".move-column-to-workspace = "w9";
+
 				};
 			};
 		};
