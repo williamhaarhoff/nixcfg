912348789296de087445cd6d2456513be3022fa6
will <williamhaarhoff@gmail.com>
2026-06-16 14:31:27 +1200

    Need a better solution for ssh keys etc.

--- modules/features/git.nix
+++ modules/features/git.nix
@@ -12,6 +12,7 @@
 			};
 		};
 
+		# todo - ssh agent does not add the keys on startup - need a solution
 		#programs.ssh.enable = true; # todo - split out ssh enable
 		programs.ssh.startAgent = true;
 		programs.ssh.extraConfig = ''
